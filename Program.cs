b7a0ed1c169280bc75ecd7ea09e978a0f7a2dc61
Kyle Rader <kyle@kylerader.ninja>
2019-08-08 23:11:59 -0700

    Init repo

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FileOptics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
