69ceaa96594c309435ba2fdb1df4280f2a8cb48f
Kyle Rader <kyle@kylerader.ninja>
2019-08-09 12:26:06 -0700

    initial hex dumper

---
+++ FileOptics/Cli.cs
@@ -0,0 +1,83 @@
+﻿using McMaster.Extensions.CommandLineUtils;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IO.Abstractions;
+using System.Linq;
+
+namespace FileOptics
+{
+    [Command(
+        Name = "fo",
+        FullName = "FileOptics",
+        Description = "A file inspector application")]
+    class Cli
+    {
+        private IConsole console;
+        private IFileSystem fs;
+
+        [Argument(0,
+            Name = "FILE",
+            Description = "The relative or absolute path to the file you want optics into.")]
+        [Required]
+        [FileExists]
+        string File { get; set; }
+
+        public Cli(IConsole console, IFileSystem fs)
+        {
+            this.console = console;
+            this.fs = fs;
+        }
+
+        IDictionary<int, string> printAs = new Dictionary<int, string>()
+        {
+            {0x0a, "\\n" },
+            {0x0d, "\\r" },
+        };
+
+        int OnExecute()
+        {
+            console.WriteLine($"File: {File}\n");
+            using (var reader = fs.File.OpenRead(File))
+            {
+                int datum;
+                while ((datum = reader.ReadByte()) != -1)
+                {
+                    console.Write("{0:X2} ({1,-3:s}", datum, StringDatum(datum) + ")");
+                    if (datum == 0x0a)
+                    {
+                        console.Write("\n");
+                    }
+                    else
+                    {
+                        console.Write(" ");
+                    }
+                }
+                console.Write("\n");
+            }
+            return 0;
+        }
+
+        string StringDatum(int datum)
+        {
+            if (printAs.ContainsKey(datum)) { return printAs[datum]; }
+
+            char c = (char)datum;
+            bool normal = new[]
+            {
+                char.IsLetterOrDigit(c),
+                char.IsPunctuation(c),
+                char.IsWhiteSpace(c),
+            }.Any();
+
+            if (normal)
+            {
+                return c.ToString();
+            }
+            else if (char.IsSeparator(c))
+            {
+                return char.GetUnicodeCategory(c).ToString();
+            }
+            return datum.ToString("X2");
+        }
+    }
+}
--- FileOptics/Program.cs
+++ FileOptics/Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using McMaster.Extensions.CommandLineUtils;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Diagnostics;
+using System.IO.Abstractions;
+using System.Reflection;
 
 namespace FileOptics
 {
@@ -6,7 +11,26 @@ namespace FileOptics
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var app = new CommandLineApplication<Cli>(throwOnUnexpectedArg: false);
+            app.Conventions.UseDefaultConventions();
+            app.HelpOption("-h|--help");
+            app.VersionOption("-v|--version", GetVersion());
+
+            app.Conventions.UseConstructorInjection(Services());
+            Environment.Exit(app.Execute(args));
+        }
+
+        static string GetVersion()
+        {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            return FileVersionInfo.GetVersionInfo(assembly.Location).ProductVersion;
+        }
+
+        static IServiceProvider Services()
+        {
+            return new ServiceCollection()
+                .AddSingleton<IFileSystem, FileSystem>()
+                .BuildServiceProvider();
         }
     }
 }
