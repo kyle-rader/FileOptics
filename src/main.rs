6fb5a0878da2b1d800afd3f4ebcb3259c15f4ad3
Kyle Rader <kylewrader@gmail.com>
2024-10-27 20:51:45 -0700

    restructure cli into commands

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,33 @@ use clap::{self, Parser};
 use file_optics::hex_print;
 
 #[derive(Debug, Parser)]
-struct Args {
+enum Cli {
+    /// Show the bytes of a file or stdin
+    Show(ShowArgs),
+
+    /// Print an ASCII table
+    Ascii(AsciiArgs),
+
+    /// Show the byte value of a single character
+    Char(CharArgs),
+}
+
+#[derive(Debug, Parser)]
+struct ShowArgs {
     file: Option<String>,
 
     #[clap(short, long)]
     /// Print readable characters
     readable: bool,
+}
+
+#[derive(Debug, Parser)]
+struct AsciiArgs {}
+
+#[derive(Debug, Parser)]
+struct CharArgs {
+    /// The char(s) to show byte values of
+    input: String,
 }
 
 /// Return an input source from either a file or stdin
@@ -21,15 +42,13 @@ fn input(file: Option<String>) -> Result<impl Read, std::io::Error> {
 }
 
 fn main() -> anyhow::Result<()> {
-    let args = Args::parse();
+    let args = Cli::parse();
 
-    if let Some(ref file) = args.file {
-        println!("Input: {:?}", file);
-    } else {
-        println!("Input: stdin");
+    match args {
+        Cli::Show(ShowArgs { file, readable }) => {
+            Ok(hex_print(input(file)?, &mut std::io::stdout(), readable)?)
+        }
+        Cli::Ascii(ascii_args) => todo!(),
+        Cli::Char(char_args) => todo!(),
     }
-
-    let input = input(args.file)?;
-
-    Ok(hex_print(input, &mut std::io::stdout(), args.readable)?)
 }
