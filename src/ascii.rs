8715bee8bc2f86f04ca6292896b26ac2cd034074
Kyle Rader <kylewrader@gmail.com>
2024-10-27 21:56:24 -0700

    add ascii table horizontal

--- src/ascii.rs
+++ src/ascii.rs
@@ -1,11 +1,46 @@
-use std::io::{self, Write};
+use std::{
+    io::{self, Write},
+    ops::RangeInclusive,
+};
 
 use crate::show;
 
+const SYMBOLS: [RangeInclusive<u8>; 4] = [33..=47, 58..=64, 91..=94, 123..=126];
+const NUMBERS: RangeInclusive<u8> = 48..=57;
+const UPPER_CASE: RangeInclusive<u8> = 65..=90;
+const LOWER_CASE: RangeInclusive<u8> = 97..=122;
+
 pub fn ascii_table(out: &mut impl Write) -> io::Result<()> {
-    for b in u8::MIN..=u8::MAX {
-        writeln!(out, "{b:000} = {}", b as char)?;
+    writeln!(out, "Symbols")?;
+    print_range(out, SYMBOLS.into_iter().flatten())?;
+
+    writeln!(out, "\n\nNumbers")?;
+    print_range(out, NUMBERS)?;
+
+    writeln!(out, "\n\nUpper Case")?;
+    print_range(out, UPPER_CASE)?;
+
+    writeln!(out, "\n\nLower Case")?;
+    print_range(out, LOWER_CASE)?;
+
+    Ok(())
+}
+
+fn print_range(out: &mut impl Write, range: impl Iterator<Item = u8>) -> io::Result<()> {
+    let (size_hint, _) = range.size_hint();
+    let mut readable = String::from("char:");
+    let mut base_10 = String::from("dec :");
+    let mut base_16 = String::from("hex :");
+
+    for b in range {
+        readable.push_str(format!(" {}  ", b as char).as_str());
+        base_10.push_str(format!("{b:-3} ").as_str());
+        base_16.push_str(format!("{b:-3X} ").as_str());
     }
+    writeln!(out, "{}", readable)?;
+    writeln!(out, "{}", base_10)?;
+    writeln!(out, "{}", base_16)?;
+
     Ok(())
 }
 
